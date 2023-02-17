249618fb06d0e696096b0b0d641eefe9abaa8844
isaac-ad <isaacadnanu@gmail.com>
2023-02-16 19:45:28 -0500

    refatored and add mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,19 @@
 const request = require('request');
-const breedName = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log(`Error: ${error}`);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log(`Breed '${breedName}' not found.`);
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(URL, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      if (data[0]) {
+        callback(null, data[0].description);
+      } else {
+        callback(`Breed '${breedName}' not found.`, null);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
